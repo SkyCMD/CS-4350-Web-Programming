08a4c7797be80a99649ab1b8d1a984c724a8ff3d
Skyler Clark <skylerclark3@SUU.EDU>
2019-10-04 14:49:47 -0600

    added lots of message socket stuff, installed app component folder

--- src/lecture/w6d1/src/app/app.component.ts
+++ src/lecture/w6d1/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { ISocketMessage } from './interfaces';
 
 @Component({
   selector: 'app-root',
@@ -6,6 +7,50 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  public message: string; //everything is public in javascript
+  public allMessages:  ISocketMessage[] = [];
   private username: string;
   private io: {emit: (key: string, value: string) => void, on: (event: string, callback: (data) => void) => void};
+  private isConnected = false; //knows it is implicity a boolean by value of false
+
+  constructor() {
+
+  }
+
+  public addParticipantsMessage(data: ISocketMessage){
+
+  }
+
+  public sendMessage(){
+
+  }
+
+  public onKeyDown(event: KeyboardEvent){
+
+  }
+  
+  public log(message:string, shouldPrepend?: boolean){
+
+  }
+
+  public addChatMessage(data: ISocketMessage){
+
+  }
+
+  public scrollToMessageTop(){
+
+  }
+
+  public getUsernameColor(username: string){
+
+  }
+
+  public inputMessageClick(event: MouseEvent){
+
+  }
+
+  private setUpListeners(){
+    
+  }
+
 }
---
+++ src/lecture/w6d1/src/app/message/message.component.ts
@@ -0,0 +1,34 @@
+import { Component, OnInit, Input, HostBinding } from '@angular/core';
+import { ISocketMessage } from '../interfaces';
+
+@Component({
+  selector: '[app-message]',
+  templateUrl: './message.component.html',
+  styles: [`
+    .username {
+      font-weight: 700;
+      overflow: hidden;
+      padding-right: 15px;
+      text-align: right;
+    }
+  `]
+})
+export class MessageComponent implements OnInit {
+  @Input('app-message') public appMessage: ISocketMessage;
+  @HostBinding('class.log') public isLog: boolean;
+  @HostBinding('class.message') public isMessage: boolean;
+  public message: string;
+  public color: string;
+  public username: string;
+
+  constructor() { }
+
+  public ngOnInit() { //should have 'everything' good to go by ngOnInIt
+    this.isLog = this.appMessage.isLog;
+    this.isMessage = this.appMessage.isLog;
+    this.color = this.appMessage.color;
+    this.message = this.appMessage.message;
+    this.username = this.appMessage.username;
+  }
+
+}
