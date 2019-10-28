6b2794cf3fee11a601cf908cfe930ff689bdaa13
Skyler Clark <skylerclark3@SUU.EDU>
2019-10-28 14:52:16 -0600

    electron lecture

--- src/lecture/w6h1/src/app/socket-messages.ts
+++
@@ -1,6 +0,0 @@
-export const SocketMessage = {
-    Welcome: "Welcome to CS 4350 chat!",
-    Disconnected: "You done just got disconnected bro.",
-    Reconnected: "You have been retentered the matrix",
-    UnableToReconnect: "Attempt to reconnect has failed."
-};
