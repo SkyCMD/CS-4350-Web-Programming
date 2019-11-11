9f36c1028ce08b8d17dbe64a77db40471ed54648
Skyler Clark <skylerclark3@SUU.EDU>
2019-11-11 14:50:26 -0700

    what

--- src/project/FinalProject/src/app/app.module.ts
+++ src/project/FinalProject/src/app/app.module.ts
@@ -4,20 +4,15 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { MatStepperModule, MatButtonModule } from '@angular/material';
-import { MatIconModule } from '@angular/material'
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    MatStepperModule,
-    MatIconModule,
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule,
-    MatButtonModule
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
